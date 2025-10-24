3019e1be2255db6aeb5a8b6487570ae9bdbf7501
NewbieXvwu <87637612+NewbieXvwu@users.noreply.github.com>
2025-10-24 13:19:22 +0800

    Wait for a icon

--- vite.config.js
+++ vite.config.js
@@ -36,12 +36,30 @@ export default defineConfig({
       // 这个文件描述了你的应用信息，比如名称、图标、主题色等
       manifest: {
         name: 'Open E→C Dictionary',
-        short_name: 'Open Dictionary',
+        short_name: 'E→C Dict',
         description: '一个开源、离线可用的英汉词典',
         start_url: '/',
         display: 'standalone',
         background_color: '#111315', // 和你的背景色一致
-        theme_color: '#111315'      // 和你的背景色一致
+        theme_color: '#111315',      // 和你的背景色一致
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable' // "maskable" 图标能更好地适应不同形状的图标蒙版
+          }
+        ]
       }
     })
   ],
