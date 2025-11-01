0a25ec588d6cbb883142ee4566eeefe2440b6e7d
NewbieXvwu <87637612+NewbieXvwu@users.noreply.github.com>
2025-11-01 23:48:39 +0800

    Create form-mappings.js

---
+++ src/modules/form-mappings.js
@@ -0,0 +1,87 @@
+// src/modules/form-mappings.js
+
+export const formKeyAliases = {
+  // --- 动词相关 ---
+  base_form: 'base_form',
+  verb_base: 'base_form',
+  infinitive: 'infinitive',
+  present_participle: 'present_participle',
+  verb_present_participle: 'present_participle',
+  gerund: 'gerund',
+  past_tense: 'past_tense',
+  verb_past_tense: 'past_tense',
+  past_participle: 'past_participle',
+  verb_past_participle: 'past_participle',
+  third_person_singular: 'third_person_singular',
+  verb_third_person_singular: 'third_person_singular',
+  present_third_person_singular: 'third_person_singular',
+  third_person_singular_present: 'third_person_singular',
+
+  // --- 名词相关 ---
+  plural: 'plural',
+  plural_form: 'plural',
+  plural_noun: 'plural',
+  singular: 'singular',
+  singular_form: 'singular',
+  
+  // --- 形容词/副词相关 ---
+  comparative: 'comparative',
+  comparative_form: 'comparative',
+  adjective_comparative: 'comparative',
+  superlative: 'superlative',
+  superlative_form: 'superlative',
+  adjective_superlative: 'superlative',
+  adjective: 'adjective',
+  adverb: 'adverb',
+  adverbial_form: 'adverb',
+
+  // --- 词性分类 ---
+  noun: 'noun',
+  verb: 'verb',
+  
+  // --- 拼写变体 ---
+  alternate_spelling: 'variant_spelling',
+  alternative_form: 'variant_spelling',
+  alternative_spelling: 'variant_spelling',
+  variant_spelling: 'variant_spelling',
+  variant_spellings: 'variant_spelling',
+  variant: 'variant_spelling',
+  variants: 'variant_spelling',
+  variant_of: 'variant_spelling',
+  us_spelling: 'us_spelling',
+  american_spelling: 'us_spelling',
+  british_spelling: 'british_spelling',
+  
+  // --- 其他 ---
+  contraction: 'contraction',
+  abbreviation: 'contraction',
+  abbreviated_form: 'contraction',
+  symbol: 'symbol',
+  root_word: 'root_word',
+};
+
+// 标准键名的翻译表
+export const formTranslations = {
+  base_form: '基本形式',
+  infinitive: '不定式',
+  present_participle: '现在分词',
+  past_tense: '过去式',
+  past_participle: '过去分词',
+  third_person_singular: '第三人称单数',
+  gerund: '动名词',
+  plural: '复数',
+  singular: '单数',
+  comparative: '比较级',
+  superlative: '最高级',
+  adjective: '形容词',
+  adverb: '副词',
+  noun: '名词',
+  verb: '动词',
+  contraction: '缩写',
+  variant_spelling: '异体拼写',
+  us_spelling: '美式拼写',
+  british_spelling: '英式拼写',
+  symbol: '符号',
+  root_word: '词根',
+  none: '无',
+};
